1fc40d40f7bdde3a5a15d7c2670f855b2bc1dab0
Sean Huvaya <sean.ktxdev@gmail.com>
2025-05-05 15:33:55 -0400

    Add commands to be implemented

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from commands.init import app as init_app
 app = typer.Typer()
 
 app.add_typer(init_app)
+# TODO Add commands for: git add <file>, git commit, git status, git diff, git log, git branch,
+#  git checkout <branch>, git merge <branch>, git rebase <branch>, git reset, git rm <file>, git stash, git clean
 
 if __name__ == "__main__":
     try:
