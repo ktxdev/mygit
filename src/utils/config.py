f840d3641b79c104648ffc080451685fff734863
Sean Huvaya <sean.ktxdev@gmail.com>
2025-05-26 02:20:33 -0400

    Implement test with invalid section

--- src/utils/config.py
+++ src/utils/config.py
@@ -6,15 +6,18 @@ from src.utils.constants import REPO_DIR_NAME, CONFIG_FILE_NAME
 class ConfigError(Exception):
     pass
 
-def add_config(key: str, value: str) -> None:
+def seperate_key_into_section_and_key(key: str) -> tuple[str, str]:
     key_parts = key.split('.')
-
     if len(key_parts) < 2:
         raise ConfigError(f"key does not contain a section: {key}")
     
     section = f"{key_parts[0]} '{'.'.join(key_parts[1: len(key_parts) - 1])}'".replace("''", "").strip()
     key = key_parts[len(key_parts) - 1]
-    
+
+    return section, key
+
+def add_config(key: str, value: str) -> None:
+    section, key = seperate_key_into_section_and_key(key)
     config_file_path = os.path.join(os.getcwd(), REPO_DIR_NAME, CONFIG_FILE_NAME)
 
     if not os.path.exists(config_file_path):
@@ -30,3 +33,18 @@ def add_config(key: str, value: str) -> None:
 
     with open(config_file_path, "w") as configfile:
         config_parser.write(configfile)
+
+def get_config(key: str) -> str:
+    section, key = seperate_key_into_section_and_key(key)
+    config_file_path = os.path.join(os.getcwd(), REPO_DIR_NAME, CONFIG_FILE_NAME)
+
+    if not os.path.exists(config_file_path):
+        raise ConfigError(f"No configurations have been set, repository may not have been initialized")
+
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file_path)
+
+    if section not in config_parser.sections():
+        raise ConfigError(f"Configuration not found")
+    
+    return config_parser[section][key]
--- tests/test_config.py
+++ tests/test_config.py
@@ -4,7 +4,7 @@ import unittest
 import tempfile
 import configparser
 
-from src.utils.config import add_config, ConfigError
+from src.utils.config import add_config, get_config, seperate_key_into_section_and_key, ConfigError
 from src.utils.constants import REPO_DIR_NAME, CONFIG_FILE_NAME
 
 class TestConfig(unittest.TestCase):
@@ -25,9 +25,9 @@ class TestConfig(unittest.TestCase):
         os.chdir(self.original_dir)
         shutil.rmtree(self.temp_dir)
         
-    def test_write_without_section_should_raise_error(self):
+    def test_seperate_key_into_section_and_key_with_no_section_should_raise_error(self):
         with self.assertRaises(ConfigError) as context:
-            add_config("email", "email@example.com")
+            seperate_key_into_section_and_key("email")
             self.assertEqual(str(context.exception), "key does not contain a section: email")
 
     def test_write_with_section_should_add_config(self):
@@ -50,22 +50,22 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(config["user"]["name"], "Sean")
         self.assertEqual(config["user"]["email"], "sean@example.com")
 
-    def test_write_with_multiple_section_separators_should_add_config(self):
-        add_config("user.name.preferred.first", "Sean")
-        config_file_path = os.path.join(os.getcwd(), REPO_DIR_NAME, CONFIG_FILE_NAME)
-        
-        config = configparser.ConfigParser()
-        config.read(config_file_path)
-
-        self.assertEqual(config["user 'name.preferred'"]["first"], "Sean")
+    def test_seperate_key_into_section_and_key_with_multiple_section_separators(self):
+        section, key = seperate_key_into_section_and_key("user.name.preferred.first")
+        self.assertEqual(section, "user 'name.preferred'")
 
     def test_write_with_no_config_file_should_raise_error(self):
         config_file_path = os.path.join(os.getcwd(), REPO_DIR_NAME, CONFIG_FILE_NAME)
         os.remove(config_file_path)
-        
+
         with self.assertRaises(ConfigError) as context:
             add_config("user.name", "Sean")
             self.assertEqual(str(context.exception), "No configurations have been set, repository may not have been initialized")
+
+    def test_get_config_with_key_containing_invalid_section_should_raise_error(self):
+        with self.assertRaises(ConfigError) as context:
+            get_config("core.repositoryformatversion")
+            self.assertEqual(str(context.exception), "Configuration not found")
 
 if __name__ == "__main__":
     unittest.main()
